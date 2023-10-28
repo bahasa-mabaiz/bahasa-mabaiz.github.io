e021c49c79c81104a1609a52d8fe4480f4eacc80
mzaini30 <muhzaini30@gmail.com>
2023-10-28 16:58:38 +0800

    simpan dulu

---
+++ src/komponen/tools-perhitungan.svelte
@@ -0,0 +1,20 @@
+<div class="pt-3">
+  <div class="form-control w-full">
+    <label class="label">
+      <span class="label-text">Paling Sedikit</span>
+    </label>
+    <input type="tel" placeholder="1" class="input input-bordered w-full" />
+  </div>
+  <div class="form-control w-full">
+    <label class="label">
+      <span class="label-text">Paling Banyak</span>
+    </label>
+    <input type="tel" placeholder="10" class="input input-bordered w-full" />
+  </div>
+  <textarea
+    class="textarea textarea-bordered w-full mt-3"
+    readonly
+    placeholder="Hasil"
+    rows="10"
+  />
+</div>
